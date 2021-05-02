74eba64824837e8e90363b7d05d5df40af5e3a52
Master-Snack <sonphukl@gmail.com>
2021-05-02 13:47:58 +0700

    connect database from postgres to s3

---
+++ extract_postgres_full.py
@@ -0,0 +1,50 @@
+import psycopg2
+import csv
+import boto3
+import configparser
+
+parser = configparser.ConfigParser()
+parser.read("pipeline.conf")
+dbname = parser.get("postgres_config", "database")
+user = parser.get("postgres_config", "username")
+password = parser.get("postgres_config", "password")
+host = parser.get("postgres_config", "host")
+port = parser.get("postgres_config", "port")
+
+conn = psycopg2.connect(
+    "dbname" + dbname
+    + " user=" + user
+    + " password=" + password
+    + " host=" + host,
+    port = port 
+)
+
+m_query = "SELECT *FROM Orders;"
+local_filename = "order_etract.csv"
+
+m_cursor = conn.cursor()
+m_cursor.execute(m_query)
+results = m_cursor.fetchall()
+
+with open(local_filename, 'w') as f:
+    csv_w = csv.writer(f, delimiter="|")
+    csv_w.writerows(results)
+
+f.close()
+m_cursor.close()
+conn.close()
+
+# load the aws_boto_credentials values
+parser = configparser.ConfigParser()
+parser.read("pipeline.conf")
+access_key = parser.get("aws_boto_credentials", "access_key")
+secret_key = parser.get("aws_boto_credentials", "secret_key")
+bucket_name = parser.get("aws_boto_credentials", "bucket_name")
+
+s3 = boto3.client('s3',
+                    aws_access_key_id=access_key,
+                    aws_secret_access_key=secret_key)
+s3_file = local_filename
+s3.upload_file(local_filename,
+                bucket_name,
+                s3_file)
