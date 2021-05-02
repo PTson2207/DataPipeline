2887bea790ac5ea696a0ab57a785541e685fbd27
Master-Snack <sonphukl@gmail.com>
2021-05-02 12:55:58 +0700

    upload data from S3 to AWS

--- extract_mysql_full.py
+++ extract_mysql_full.py
@@ -37,3 +37,17 @@ with open(local_filename, 'w') as f:
 f.close()
 m_cursor.close()
 conn.close()
+
+# load the awskey_boto_credentials values
+parser = configparser.ConfigParser()
+parser.read("pipeline.conf")
+access_key = parser.get("aws_boto_credentials", "access_key")
+secret_key = parser.get("aws_boto_credentials", "secret_key")
+bucket_key = parser.get("aws_boto_credentials", "bucket_name")
+
+
+s3 = boto3.client('s3', aws_access_key_id=access_key,
+                        aws_secret_access_key=secret_key)
+
+s3_file = local_filename
+s3.upload_file(local_filename, bucket_key, s3_file)
